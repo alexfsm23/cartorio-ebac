72e529f1066a8a30b70071f67aec8a0e46a3aa0e
Alex Fernando <alexfsmdev23@gmail.com>
2023-02-17 10:37:50 -0300

    adicionando estruturas de repeticao no menu

--- cartorio.c
+++ cartorio.c
@@ -5,32 +5,47 @@
 int main (){
 	
 	int option = 0; // Definindo variáveis
-	
-	setlocale(LC_ALL, "Portuguese"); // Definindo Idioma do Programa
-	
-	
-	printf("### Cartório da EBAC ###\n\n"); // Início do Menu
-	printf("Escolha a opção desejada do menu: \n\n");
-	printf("\t1 - Registrar Nomes\n");
-	printf("\t2 -Consultar Nomes\n");
-	printf("\t3 - Deletar Nomes\n\n"); 
-	printf("Opção: "); // Fim do Menu
 
-	scanf("%d", & option); // Armazenando a escolha do usuário
+	int count = 1;
+	for (count = 1; count = 1;){
+		
+		system("cls");
+		
+		setlocale(LC_ALL, "Portuguese"); // Definindo Idioma do Programa
 	
-	system("cls");
 	
-	// Início da Validação
+		printf("### Cartório da EBAC ###\n\n"); // Início do Menu
+		printf("Escolha a opção desejada do menu: \n\n");
+		printf("\t1 - Registrar Nomes\n");
+		printf("\t2 -Consultar Nomes\n");
+		printf("\t3 - Deletar Nomes\n\n"); 
+		printf("Opção: "); // Fim do Menu
 	
-	if (option == 1){ 
-		printf("Você escolheu o Registro de Nomes!");
-	} else if (option == 2){
-		printf("Você escolheu Consultar Nomes!");
-	} else if (option == 3){
-		printf("Você escolheu Deletar Nomes!");
-	} else {
-		printf("Você escolheu uma opção que não existe no Menu!");
+		scanf("%d", & option); // Armazenando a escolha do usuário
+		
+		system("cls");
+		
+		// Início da Validação
+
+		switch (option){
+			case 1:
+				printf("Você escolheu o Registro de Nomes!\n");
+				system("pause");
+			break;
+			case 2:
+				printf("Você escolheu Consultar Nomes!\n");
+				system("pause");
+			break;
+			case 3:
+				printf("Você escolheu Deletar Nomes!\n");
+				system("pause");
+			break;
+			default:
+				printf("Você escolheu uma opção que não existe no Menu!\n");
+				system("pause");
+			break;	
+		}		
+		
+		// Fim da Validação	
 	}
-	
-	// Fim da Validação
 }
